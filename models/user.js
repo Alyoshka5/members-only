8c74dff711e675314448abd9548883117a7a73a3
Alyoshka <alyoshkamyshko@gmail.com>
2023-09-02 09:11:54 -0700

    Add user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const UserSchema = new Schema({
+    firstName: { type: String, required: true, maxLength: 50 },
+    lastName: { type: String, required: true, maxLength: 50 },
+    email: { type: String, required: true, validate: [validateEmail, 'Please provide a valid email address'] },
+    password: { type: String, required: true },
+    status: { type: String, required: true },
+    isAdmin: Boolean
+});
+
+UserSchema.virtual('ur').get(function() {
+    return `/users/${this._id}`;
+});
+
+module.exports = mongoose.model('User', UserSchema);
