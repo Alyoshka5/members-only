ddfa717fd69ac89d6a7494acbc430281a8773df6
Alyoshka <alyoshkamyshko@gmail.com>
2023-09-02 16:28:47 -0700

    Install and initialize passport

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
@@ -30,6 +34,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
