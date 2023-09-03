b2fc98458135b6db3d4746149ab967e38b0a6ea5
Alyoshka <alyoshkamyshko@gmail.com>
2023-09-03 16:19:31 -0700

    Move post and user routes to index router

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const postsRouter = require('./routes/posts');
 
 const User = require('./models/user');
 
@@ -78,10 +76,7 @@ app.use(function (req, res, next) {
   next();
 })
 
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,27 @@ const userController = require('../controllers/userController');
 const postController = require('../controllers/postController');
 
 
-/* GET home page. */
+// posts
 router.get('/', postController.postList);
 
+router.get('/post/create', postController.postCreateGet);
+
+router.post('/post/create', postController.postCreatePost);
+
+router.get('/post/:id/update', postController.postUpdateGet);
+
+router.post('/post/:id/update', postController.postUpdatePost);
+
+router.get('/post/:id/delete', postController.postDeleteGet);
+
+router.post('/post/:id/delete', postController.postDeletePost);
+
+router.get('/post/:id', postController.postDetail);
+
+router.get('/posts', postController.postList);
+
+
+// users
 router.get('/sign-up', userController.signUpGet);
 
 router.post('/sign-up', userController.signUpPost);
