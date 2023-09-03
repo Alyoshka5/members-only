c6845c0cfe0708b27f05830ff1de7e2c412c2a2f
Alyoshka <alyoshkamyshko@gmail.com>
2023-09-03 16:24:33 -0700

    Add default post controller methods

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,33 @@
 const asyncHandler = require('express-async-handler');
 
-exports.postList = asyncHandler(async (req, res, next) => {
+exports.list = asyncHandler(async (req, res, next) => {
     res.render('posts/list', { title: 'Members Only' });
+});
+
+exports.detail = asyncHandler(async (req, res, next) => {
+    res.send('');
+});
+
+exports.createGet = asyncHandler(async (req, res, next) => {
+    res.send('');
+});
+
+exports.createPost = asyncHandler(async (req, res, next) => {
+    res.send('');
+});
+
+exports.updateGet = asyncHandler(async (req, res, next) => {
+    res.send('');
+});
+
+exports.updatePost = asyncHandler(async (req, res, next) => {
+    res.send('');
+});
+
+exports.deleteGet = asyncHandler(async (req, res, next) => {
+    res.send('');
+});
+
+exports.deletePost = asyncHandler(async (req, res, next) => {
+    res.send('');
 });
