06185093f8e105f45b97b9a1059ca35b72c8e165
Alyoshka <alyoshkamyshko@gmail.com>
2023-09-03 17:20:02 -0700

    Add post create handler functions

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+
+const Post = require('../models/post');
 
 exports.list = asyncHandler(async (req, res, next) => {
     res.render('posts/list', { title: 'Members Only' });
@@ -9,12 +12,48 @@ exports.detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.createGet = asyncHandler(async (req, res, next) => {
-    res.send('');
+    if (req.user)
+        res.render('posts/form');
+    else
+        res.redirect('/');
 });
 
-exports.createPost = asyncHandler(async (req, res, next) => {
-    res.send('');
-});
+exports.createPost = [
+    body('title')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('You must include a title')
+        .isLength({ max: 150 })
+        .withMessage('Title cannot be longer than 150 characters')
+        .escape(),
+
+    body('content', 'You must include a message in your post')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const post = new Post({
+            author: req.user._id,
+            title: req.body.title,
+            content: req.body.content
+        });
+
+        if (!errors.isEmpty()) {
+            res.render('posts/form', {
+                title: 'Members Only',
+                post,
+                errors: errors.array()
+            });
+            return;
+        }
+
+        await post.save();
+        res.redirect('/');
+    })
+]
 
 exports.updateGet = asyncHandler(async (req, res, next) => {
     res.send('');
