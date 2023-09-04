699c7548e59ec92732d23aa123862ab766688970
Alyoshka <alyoshkamyshko@gmail.com>
2023-09-03 18:50:40 -0700

    Add update handler functions

--- controllers/postController.js
+++ controllers/postController.js
@@ -73,12 +73,60 @@ exports.createPost = [
 ]
 
 exports.updateGet = asyncHandler(async (req, res, next) => {
-    res.send('');
+    const post = await Post.findById(req.params.id).exec();
+
+    if (req.user._id.toString() !== post.author.toString())
+        return res.redirect(post.url);
+
+    if (post === null) {
+        const err = new Error('Post not found');
+        err.status = 404;
+        next(err);
+    }
+
+    res.render('posts/form', {
+        title: 'Update Post',
+        post
+    });
 });
 
-exports.updatePost = asyncHandler(async (req, res, next) => {
-    res.send('');
-});
+exports.updatePost = [
+    body('title')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('You must include a title')
+        .isLength({ max: 150 })
+        .withMessage('Title cannot be longer than 150 characters')
+        .escape(),
+
+    body('content', 'You must include a message in your post')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const post = new Post({
+            author: req.user._id,
+            title: req.body.title,
+            content: req.body.content,
+            _id: req.params.id
+        });
+
+        if (!errors.isEmpty()) {
+            res.render('posts/form', {
+                title: 'Create Post',
+                post,
+                errors: errors.array()
+            });
+            return;
+        }
+
+        await Post.findByIdAndUpdate(req.params.id, post, {});
+        res.redirect(post.url);
+    })
+]
 
 exports.deleteGet = asyncHandler(async (req, res, next) => {
     res.send('');
